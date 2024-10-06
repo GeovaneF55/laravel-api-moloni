ab15b3bc9f1b573cd641c6fe18f2c0adc1e2f887
GeovaneF55 <geovanefss@gmail.com>
2024-10-06 01:33:24 +0100

    Finished SupplierWarrantyRequests

--- src/Http/Documents/SupplierWarrantyRequests.php
+++ src/Http/Documents/SupplierWarrantyRequests.php
@@ -28,7 +28,15 @@ class SupplierWarrantyRequests extends ApiAbstract
         $endpoint = $this->getEndpoint('count/');
         
         $rules = [
-            // TODO
+            'company_id' => ['required', 'numeric'],
+            'supplier_id' => ['numeric'],
+            'document_set_id' => ['numeric'],
+            'number' => ['numeric'],
+            'date' => ['date'],
+            'expiration_date' => ['date'],
+            'year' => ['numeric'],
+            'your_reference' => ['string'],
+            'our_reference' => ['string']
         ];
 
         $this->apiClient->validate($rules, $data);
@@ -48,7 +56,17 @@ class SupplierWarrantyRequests extends ApiAbstract
         $endpoint = $this->getEndpoint('getAll/');
         
         $rules = [
-            // TODO
+            'company_id' => ['required', 'numeric'],
+            'qty' => ['numeric'],
+            'offset' => ['numeric'],
+            'supplier_id' => ['numeric'],
+            'document_set_id' => ['numeric'],
+            'number' => ['numeric'],
+            'date' => ['date'],
+            'expiration_date' => ['date'],
+            'year' => ['numeric'],
+            'your_reference' => ['string'],
+            'our_reference' => ['string']
         ];
 
         $this->apiClient->validate($rules, $data);
@@ -68,7 +86,16 @@ class SupplierWarrantyRequests extends ApiAbstract
         $endpoint = $this->getEndpoint('getOne/');
         
         $rules = [
-            // TODO
+            'company_id' => ['required', 'numeric'],
+            'document_id' => ['numeric'],
+            'supplier_id' => ['numeric'],
+            'document_set_id' => ['numeric'],
+            'number' => ['numeric'],
+            'date' => ['date'],
+            'expiration_date' => ['date'],
+            'year' => ['numeric'],
+            'your_reference' => ['string'],
+            'our_reference' => ['string']
         ];
 
         $this->apiClient->validate($rules, $data);
@@ -88,7 +115,72 @@ class SupplierWarrantyRequests extends ApiAbstract
         $endpoint = $this->getEndpoint('insert/');
         
         $rules = [
-            // TODO
+            'company_id' => ['required', 'numeric'],
+            'date' => ['required', 'date'],
+            'expiration_date' => ['required', 'date'],
+            'document_set_id' => ['required', 'numeric'],
+            'supplier_id' => ['required', 'numeric'],
+            'our_reference' => ['string'],
+            'your_reference' => ['string'],
+            'financial_discount' => ['float', 'min:0', 'max:100'],
+            'associated_documents' => ['array'],
+            'associated_documents.associated_id' => ['required', 'numeric'],
+            'associated_documents.value' => ['required', 'float'],
+            'related_documents_notes' => ['string'],
+            'products' => ['required', 'array'],
+            'products.product_id' => ['required', 'numeric'],
+            'products.name' => ['required', 'string'],
+            'products.summary' => ['required', 'string'],
+            'products.qty' => ['required', 'float'],
+            'products.price' => ['required', 'float'],
+            'products.discount' => ['float', 'min:0', 'max:100'],
+            'products.deduction_id' => ['numeric'],
+            'products.order' => ['numeric'],
+            'products.origin_id' => ['numeric'],
+            'products.exemption_reason' => ['string'],
+            'products.warehouse_id' => ['numeric'],
+            'products.has_stock' => ['boolean'],
+            'products.taxes' => ['array'],
+            'products.taxes.tax_id' => ['required', 'numeric'],
+            'products.taxes.value' => ['float'],
+            'products.taxes.order' => ['numeric'],
+            'products.taxes.cumulative' => ['numeric'],
+            'products.child_products' => ['array'],
+            'products.child_products.product_id' => ['required', 'numeric'],
+            'products.child_products.name' => ['required', 'string'],
+            'products.child_products.summary' => ['required', 'string'],
+            'products.child_products.qty' => ['required', 'float'],
+            'products.child_products.price' => ['required', 'float'],
+            'products.child_products.discount' => ['float', 'min:0', 'max:100'],
+            'products.child_products.deduction_id' => ['numeric'],
+            'products.child_products.order' => ['numeric'],
+            'products.child_products.origin_id' => ['numeric'],
+            'products.child_products.exemption_reason' => ['string'],
+            'products.child_products.warehouse_id' => ['numeric'],
+            'products.child_products.has_stock' => ['boolean'],
+            'products.child_products.properties' => ['array'],
+            'products.child_products.properties.title' => ['string'],
+            'products.child_products.properties.value' => ['string'],
+            'products.child_products.taxes' => ['array'],
+            'products.child_products.taxes.tax_id' => ['required', 'numeric'],
+            'products.child_products.taxes.value' => ['float'],
+            'products.child_products.taxes.order' => ['numeric'],
+            'products.child_products.taxes.cumulative' => ['numeric'],
+            'delivery_method_id' => ['numeric'],
+            'delivery_datetime' => ['required', 'date'],
+            'delivery_departure_address' => ['string'],
+            'delivery_departure_city' => ['string'],
+            'delivery_departure_zip_code' => ['string'],
+            'delivery_departure_country' => ['numeric'],
+            'delivery_destination_address' => ['string'],
+            'delivery_destination_city' => ['string'],
+            'delivery_destination_zip_code' => ['string'],
+            'delivery_destination_country' => ['numeric'],
+            'vehicle_id' => ['numeric'],
+            'vehicle_name' => ['string'],
+            'vehicle_number_plate' => ['string'],
+            'notes' => ['string'],
+            'status' => ['numeric']
         ];
 
         $this->apiClient->validate($rules, $data);
@@ -108,7 +200,73 @@ class SupplierWarrantyRequests extends ApiAbstract
         $endpoint = $this->getEndpoint('update/');
         
         $rules = [
-            // TODO
+            'company_id' => ['required', 'numeric'],
+            'document_id' => ['required', 'numeric'],
+            'date' => ['required', 'date'],
+            'expiration_date' => ['required', 'date'],
+            'document_set_id' => ['required', 'numeric'],
+            'supplier_id' => ['required', 'numeric'],
+            'our_reference' => ['string'],
+            'your_reference' => ['string'],
+            'financial_discount' => ['float', 'min:0', 'max:100'],
+            'associated_documents' => ['array'],
+            'associated_documents.associated_id' => ['required', 'numeric'],
+            'associated_documents.value' => ['required', 'float'],
+            'related_documents_notes' => ['string'],
+            'products' => ['required', 'array'],
+            'products.product_id' => ['required', 'numeric'],
+            'products.name' => ['required', 'string'],
+            'products.summary' => ['required', 'string'],
+            'products.qty' => ['required', 'float'],
+            'products.price' => ['required', 'float'],
+            'products.discount' => ['float', 'min:0', 'max:100'],
+            'products.deduction_id' => ['numeric'],
+            'products.order' => ['numeric'],
+            'products.origin_id' => ['numeric'],
+            'products.exemption_reason' => ['string'],
+            'products.warehouse_id' => ['numeric'],
+            'products.has_stock' => ['boolean'],
+            'products.taxes' => ['array'],
+            'products.taxes.tax_id' => ['required', 'numeric'],
+            'products.taxes.value' => ['float'],
+            'products.taxes.order' => ['numeric'],
+            'products.taxes.cumulative' => ['numeric'],
+            'products.child_products' => ['array'],
+            'products.child_products.product_id' => ['required', 'numeric'],
+            'products.child_products.name' => ['required', 'string'],
+            'products.child_products.summary' => ['required', 'string'],
+            'products.child_products.qty' => ['required', 'float'],
+            'products.child_products.price' => ['required', 'float'],
+            'products.child_products.discount' => ['float', 'min:0', 'max:100'],
+            'products.child_products.deduction_id' => ['numeric'],
+            'products.child_products.order' => ['numeric'],
+            'products.child_products.origin_id' => ['numeric'],
+            'products.child_products.exemption_reason' => ['string'],
+            'products.child_products.warehouse_id' => ['numeric'],
+            'products.child_products.has_stock' => ['boolean'],
+            'products.child_products.properties' => ['array'],
+            'products.child_products.properties.title' => ['string'],
+            'products.child_products.properties.value' => ['string'],
+            'products.child_products.taxes' => ['array'],
+            'products.child_products.taxes.tax_id' => ['required', 'numeric'],
+            'products.child_products.taxes.value' => ['float'],
+            'products.child_products.taxes.order' => ['numeric'],
+            'products.child_products.taxes.cumulative' => ['numeric'],
+            'delivery_method_id' => ['numeric'],
+            'delivery_datetime' => ['required', 'date'],
+            'delivery_departure_address' => ['string'],
+            'delivery_departure_city' => ['string'],
+            'delivery_departure_zip_code' => ['string'],
+            'delivery_departure_country' => ['numeric'],
+            'delivery_destination_address' => ['string'],
+            'delivery_destination_city' => ['string'],
+            'delivery_destination_zip_code' => ['string'],
+            'delivery_destination_country' => ['numeric'],
+            'vehicle_id' => ['numeric'],
+            'vehicle_name' => ['string'],
+            'vehicle_number_plate' => ['string'],
+            'notes' => ['string'],
+            'status' => ['numeric']
         ];
 
         $this->apiClient->validate($rules, $data);
@@ -128,7 +286,8 @@ class SupplierWarrantyRequests extends ApiAbstract
         $endpoint = $this->getEndpoint('delete/');
         
         $rules = [
-            // TODO
+            'company_id' => ['required', 'numeric'],
+            'document_id' => ['required', 'numeric']
         ];
 
         $this->apiClient->validate($rules, $data);
